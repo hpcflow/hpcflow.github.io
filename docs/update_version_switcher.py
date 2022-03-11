94915dc9a8b9d23b304c855912f7446f6703d0db
aplowman <adam.jp@live.co.uk>
2022-03-11 20:22:17 +0000

    add version switcher

---
+++ docs/update_version_switcher.py
@@ -0,0 +1,29 @@
+import json
+import sys
+
+def update(switcher_path, branch, new_version):
+
+    with open(switcher_path, 'r') as fh:
+        switches = json.load(fh)
+
+    with open(switcher_path, 'w') as fh:
+        switches_new = [
+            {
+                'name': f"{new_version} {'(stable)' if branch == 'main' else '(dev)'}",
+                "version": new_version.lstrip('v'),
+            }
+        ]
+        for i in switches:
+            if 'name' in i:
+                if '(stable)' in i['name'] and branch == 'main':
+                    i.pop('name')
+                elif '(dev)' in i['name'] and branch == 'develop':
+                    i.pop('name')                
+            switches_new.append(i)
+        
+        json.dump(switches_new, fh, indent=4)
+
+if __name__ == '__main__':    
+    update(*sys.argv[1:])
+
+
