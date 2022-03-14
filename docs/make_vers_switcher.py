59860ade9193d0270e6dbe1551d9c89333f08662
aplowman <adam.jp@live.co.uk>
2022-03-14 11:31:40 +0000

    fix switcher again3

--- docs/make_vers_switcher.py
+++ docs/make_vers_switcher.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 docs_dir = Path(__file__).parent.resolve()
 all_vers = sorted(
-    (i.name for i in docs_dir.glob('*') if i.is_dir()),
+    (i.name for i in docs_dir.glob('*') if i.is_dir() and not i.is_symlink()),
     key=lambda i: i if not i[-1].isdigit() else i + 'z'
 )
 vers_switcher = [
